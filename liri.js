c0339524413c943c5f4cce853681a2b33057927c
Omar Luna <mrebel62@gmail.com>
2019-11-11 23:01:28 -0600

    basic structure

--- liri.js
+++ liri.js
@@ -1,3 +1,29 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var axios = require('Axios');
+
 var spotify = new Spotify(keys.spotify);
+var option = process.argv[2];
+var params = process.argv.slice(3).join(" ");
+
+//Switch statement to handle options
+switch (option) {
+    case 'concert-this':
+        
+        break;
+    
+    case 'spotify-this-song':
+        break;
+    
+    case 'movie-this':
+        break;
+
+    case 'do-what-it-says':
+        break;
+
+    default:
+        console.log('Invalid option selected');
+        break;
+}
+
