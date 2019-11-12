1b12f8a189ebafc7cad0abfae75e3e2045d7fd65
Omar Luna <mrebel62@gmail.com>
2019-11-12 00:24:54 -0600

    OMDB functionality;

--- liri.js
+++ liri.js
@@ -9,11 +9,12 @@ var params = process.argv.slice(3).join("+");
 
 var concertUrlStart = "https://rest.bandsintown.com/artists/"; 
 var concertUrlEnd = "/events?app_id=codingbootcamp";
+var moviesUrl = 'http://www.omdbapi.com/?apikey=trilogy&t=';
 
 //Switch statement to handle options
 switch (option) {
     case 'concert-this':
-        
+            requestApi('concert', params);
         break;
     
     case 'spotify-this-song':
@@ -21,6 +22,7 @@ switch (option) {
         break;
     
     case 'movie-this':
+        requestApi('movie', params);
         break;
 
     case 'do-what-it-says':
@@ -52,5 +54,39 @@ function spotifyThis(params){
 }
 
 function requestApi(option, params){
-
+    switch (option){
+        case 'movie':
+            var params = (params === '') ? 'Mr. Nobody' : params;
+            axios.get(moviesUrl+params).then(response => {
+                console.log(`Movie title: ${response.data.Title}`);
+                console.log(`Release date: ${response.data.Released}`);
+                console.log(`Rotten Tomatoes rating: ${response.data.Ratings[1].Value}`);
+                console.log(`Country of origin: ${response.data.Country}`);
+                console.log(`Language: ${response.data.Language}`);
+                console.log(`Language: ${response.data.Plot}`);
+                console.log(`Language: ${response.data.Actors}`);
+            }).catch(error => {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                  } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                  } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                  }
+                  console.log(error.config);
+            });
+            break;
+        case 'concert':
+            break;
+    }
 }
