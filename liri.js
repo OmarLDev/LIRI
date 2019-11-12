9bcf04bb83d221d290e39ba14824e0baebe802dd
Omar Luna <mrebel62@gmail.com>
2019-11-12 00:01:56 -0600

    spotify functionality

--- liri.js
+++ liri.js
@@ -5,7 +5,10 @@ var axios = require('Axios');
 
 var spotify = new Spotify(keys.spotify);
 var option = process.argv[2];
-var params = process.argv.slice(3).join(" ");
+var params = process.argv.slice(3).join("+");
+
+var concertUrlStart = "https://rest.bandsintown.com/artists/"; 
+var concertUrlEnd = "/events?app_id=codingbootcamp";
 
 //Switch statement to handle options
 switch (option) {
@@ -14,6 +17,7 @@ switch (option) {
         break;
     
     case 'spotify-this-song':
+        spotifyThis(params);
         break;
     
     case 'movie-this':
@@ -27,3 +31,26 @@ switch (option) {
         break;
 }
 
+//functions to handle requests
+function spotifyThis(params){
+    params = (params === "") ? 'The sign' : params;
+    
+    spotify.search({ type: 'track', 
+                    query: params }, 
+        (err, data) => {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        data.tracks.items.forEach((element, index) => {
+            console.log(`------------------------------------`);
+            console.log(`Artist: ${element.artists[0].name}`);
+            console.log(`Song name: ${element.name}`);
+            console.log(`Link to the song: ${element.external_urls.spotify}`);
+            console.log(`Album: ${element.album.name}`);
+        });
+      });
+}
+
+function requestApi(option, params){
+
+}
